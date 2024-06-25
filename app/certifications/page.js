80a250e0ee82f793839751f9cdb5fb710cae9258
Ibrahim ADLANI <ibrahim@adlani.com>
2024-06-25 16:12:17 +0200

    Fixed the mobile version of certification page.

--- app/certifications/page.js
+++ app/certifications/page.js
@@ -9,7 +9,6 @@ import Head from "next/head";
  */
 
 export const Home = () => {
-
   const certifications = [
     {
       year: 2023,
@@ -17,7 +16,7 @@ export const Home = () => {
       madeAt: "IBM",
       builtWith: ["CyberSec", "Cloud"],
       link: "https://ibm-learning.udemy.com/certificate/UC-550b9f69-f7e3-4d14-9ffb-f9a6d6d813d7/",
-      label: "Certification"
+      label: "Certification",
     },
     {
       year: 2021,
@@ -25,7 +24,7 @@ export const Home = () => {
       madeAt: "freeCodeCamp",
       builtWith: ["JavaScript", "Statistical Anlaysis", "D3.js"],
       link: "https://freecodecamp.org/certification/ibrahimadlani/data-visualization",
-      label: "Certification"
+      label: "Certification",
     },
     {
       year: 2021,
@@ -33,7 +32,7 @@ export const Home = () => {
       madeAt: "freeCodeCamp",
       builtWith: ["JavaScript", "HTML/CSS", "DOM"],
       link: "https://freecodecamp.org/certification/ibrahimadlani/javascript-algorithms-and-data-structures",
-      label: "Certification"
+      label: "Certification",
     },
     {
       year: 2021,
@@ -41,23 +40,26 @@ export const Home = () => {
       madeAt: "freeCodeCamp",
       builtWith: ["Python", "Algorithms", "Data Structures"],
       link: "https://freecodecamp.org/certification/ibrahimadlani/javascript-algorithms-and-data-structures",
-      label: "Certification"
-    }
-  ]
+      label: "Certification",
+    },
+  ];
 
   // Meta information for the Head component
   const metaTags = {
     title: "Ibrahim Adlani - Software Engineer",
-    description: "Ibrahim Adlani's portfolio showcasing projects and skills in software engineering.",
+    description:
+      "Ibrahim Adlani's portfolio showcasing projects and skills in software engineering.",
     viewport: "width=device-width, initial-scale=1",
     ogTitle: "Ibrahim Adlani - Software Engineer",
-    ogDescription: "Explore the projects and skills of Ibrahim Adlani, a software engineer specialized in Django, Restful APIs, Python, and JavaScript.",
+    ogDescription:
+      "Explore the projects and skills of Ibrahim Adlani, a software engineer specialized in Django, Restful APIs, Python, and JavaScript.",
     ogImage: "/images/profile.jpg",
     ogUrl: "https://www.ibrahimadlani.com",
     twitterCard: "summary_large_image",
     twitterTitle: "Ibrahim Adlani - Software Engineer",
-    twitterDescription: "Explore the projects and skills of Ibrahim Adlani, a software engineer specialized in Django, Restful APIs, Python, and JavaScript.",
-    twitterImage: "/images/profile.jpg"
+    twitterDescription:
+      "Explore the projects and skills of Ibrahim Adlani, a software engineer specialized in Django, Restful APIs, Python, and JavaScript.",
+    twitterImage: "/images/profile.jpg",
   };
 
   return (
@@ -72,7 +74,10 @@ export const Home = () => {
         <meta property="og:url" content={metaTags.ogUrl} />
         <meta name="twitter:card" content={metaTags.twitterCard} />
         <meta name="twitter:title" content={metaTags.twitterTitle} />
-        <meta name="twitter:description" content={metaTags.twitterDescription} />
+        <meta
+          name="twitter:description"
+          content={metaTags.twitterDescription}
+        />
         <meta name="twitter:image" content={metaTags.twitterImage} />
       </Head>
       <div className="lg:py-24">
@@ -120,7 +125,10 @@ export const Home = () => {
           </thead>
           <tbody>
             {certifications.map((item, index) => (
-              <tr key={index} className="border-b border-muted/10 last:border-none">
+              <tr
+                key={index}
+                className="border-b border-muted/10 last:border-none"
+              >
                 <td className="py-4 pr-4 align-top text-sm">
                   <div className="translate-y-px">{item.year}</div>
                 </td>
@@ -135,25 +143,20 @@ export const Home = () => {
                         aria-label={`${item.project} (opens in a new tab)`}
                       >
                         <span>
-                          {item.project.split(" ").map((word, i) => (
-                            <span key={i} className="inline-block">
-                              {word}
-                              {i < item.project.split(" ").length - 1 && " "}
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                                aria-hidden="true"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                  clipRule="evenodd"
-                                ></path>
-                              </svg>
-                            </span>
-                          ))}
+                          {item.project}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
                         </span>
                       </a>
                     </div>
@@ -161,7 +164,9 @@ export const Home = () => {
                   </div>
                 </td>
                 <td className="hidden py-4 pr-4 align-top text-sm lg:table-cell">
-                  <div className="translate-y-px whitespace-nowrap">{item.madeAt}</div>
+                  <div className="translate-y-px whitespace-nowrap">
+                    {item.madeAt}
+                  </div>
                 </td>
                 <td className="hidden py-4 pr-4 align-top lg:table-cell">
                   <ul className="flex -translate-y-1.5 flex-wrap">
